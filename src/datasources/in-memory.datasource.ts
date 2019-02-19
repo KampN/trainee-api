16c3e9cf0fb2cf04ad22b318269652d6230bdd1a
PillowPillow <gaignoux.nicolas@gmail.com>
2019-02-19 15:31:00 +0100

    add build process

--- src/datasources/in-memory.datasource.ts
+++ src/datasources/in-memory.datasource.ts
@@ -1,14 +1,18 @@
 import {inject} from '@loopback/core';
 import {juggler} from '@loopback/repository';
-import * as config from './in-memory.datasource.json';
+import path from 'path';
+
+const DBConfig = {
+	'name': 'InMemory',
+	'connector': 'memory',
+	'localStorage': 'db',
+	'file': path.join(__dirname, '..', '..', '/db/db.json')
+};
 
 export class InMemoryDataSource extends juggler.DataSource {
 	static dataSourceName = 'InMemory';
 
-	constructor(
-		@inject('datasources.config.InMemory', {optional: true})
-			dsConfig:object = config,
-	) {
+	constructor(@inject('datasources.config.InMemory', {optional: true}) dsConfig:object = DBConfig) {
 		super(dsConfig);
 	}
 }
